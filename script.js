fd19af5b1c39d4c5c930cd16f2e01e24cee4547c
David Waltenburg <david@waltenburg.dk>
2022-10-12 01:28:17 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const hideObserver = new IntersectionObserver(entries => {
+    entries.forEach((entry) => {
+        let dot = document.querySelectorAll('.scrollDot').item(entry.target.getAttribute('id').slice(1));
+        let previndex = entry.target.getAttribute('id').slice(1)-1;
+        let nextindex = parseInt(entry.target.getAttribute('id').slice(1))+1;
+
+        previndex = previndex < 0 ? 0 : previndex;
+        nextindex = nextindex > 5 ? 5 : nextindex;
+        let predef = "#s"
+
+        let linkstringup = predef.concat(previndex);
+        let linkstringdown = predef.concat(nextindex);
+        
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+            dot.classList.add('current');
+            document.getElementById("uplink").setAttribute("href", linkstringup);
+            document.getElementById("downlink").setAttribute("href", linkstringdown);
+        }
+        else{
+            entry.target.classList.remove('show');
+            dot.classList.remove('current');
+        };
+    });
+}, {threshold:0.015});
+
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => hideObserver.observe(el));
