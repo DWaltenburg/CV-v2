f0ba701a2c11d56b9b7a27070322cd04d5dde87c
David Waltenburg <david@waltenburg.dk>
2023-09-28 17:06:19 +0200

    Added text and expanded theme toggle to start with browser preference

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-function toggleDarkmode() {
+function toggleTheme() {
     if (document.documentElement.getAttribute('data-bs-theme') === 'dark') {
         setTheme('light')
         localStorage.setItem('theme', 'light')
@@ -14,7 +14,10 @@ function toggleDarkmode() {
 const setTheme = theme => {
     if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
         document.documentElement.setAttribute('data-bs-theme', 'dark')
+        document.getElementById('brandlogo').src = "Images/GitHub-Mark-Light-120px-plus.png"
+        document.getElementById('themeToggler').checked = true
     } else {
         document.documentElement.setAttribute('data-bs-theme', theme)
     }
 }
+setTheme(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'auto')
