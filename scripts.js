48fc92c557fc13b49107dc365b5edb1bc059bd68
David Waltenburg <david@waltenburg.dk>
2023-10-01 21:26:31 +0200

    Changed image sources from cdn to local files

--- scripts.js
+++ scripts.js
@@ -3,17 +3,17 @@ function toggleTheme() {
         setTheme('light')
         localStorage.setItem('theme', 'light')
         document.getElementById('brandlogo').src = "Images/GitHubLogo.png"
-        document.getElementById('cvIcon').src = "https://img.icons8.com/ios/500/parse-from-clipboard.png"
-        document.getElementById('projectIcon').src = "https://img.icons8.com/dotty/500/project-management.png"
-        document.getElementById('profileIcon').src = "https://img.icons8.com/ios/500/user-male-circle--v2.png"
+        document.getElementById('cvIcon').src = "Images/cv-dark.png"
+        document.getElementById('projectIcon').src = "Images/project-dark.png"
+        document.getElementById('profileIcon').src = "Images/profile-dark.png"
         return
     } else {
         setTheme('dark')
         localStorage.setItem('theme', 'dark')
         document.getElementById('brandlogo').src = "Images/GitHub-Mark-Light-120px-plus.png"
-        document.getElementById('cvIcon').src = "https://img.icons8.com/ios/500/dee2e6/parse-from-clipboard.png"
-        document.getElementById('projectIcon').src = "https://img.icons8.com/dotty/500/dee2e6/project-management.png"
-        document.getElementById('profileIcon').src = "https://img.icons8.com/ios/500/dee2e6/user-male-circle--v2.png"
+        document.getElementById('cvIcon').src = "Images/cv-light.png"
+        document.getElementById('projectIcon').src = "Images/project-light.png"
+        document.getElementById('profileIcon').src = "Images/profile-light.png"
         return
     }
 }
@@ -21,9 +21,9 @@ const setTheme = theme => {
     if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
         document.documentElement.setAttribute('data-bs-theme', 'dark')
         document.getElementById('brandlogo').src = "Images/GitHub-Mark-Light-120px-plus.png"
-        document.getElementById('cvIcon').src = "https://img.icons8.com/ios/500/dee2e6/parse-from-clipboard.png"
-        document.getElementById('projectIcon').src = "https://img.icons8.com/dotty/500/dee2e6/project-management.png"
-        document.getElementById('profileIcon').src = "https://img.icons8.com/ios/500/dee2e6/user-male-circle--v2.png"
+        document.getElementById('cvIcon').src = "Images/cv-light.png"
+        document.getElementById('projectIcon').src = "Images/project-light.png"
+        document.getElementById('profileIcon').src = "Images/profile-light.png"
         document.getElementById('themeToggler').checked = true
     } else {
         document.documentElement.setAttribute('data-bs-theme', theme)
